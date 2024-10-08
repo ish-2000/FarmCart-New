17d4f560e531c863a155e8983d20bef5eb8629b6
ishara <160392609+ish-2000@users.noreply.github.com>
2024-10-08 13:16:18 +0530

    done

--- frontend/src/Pages/Shop/ProductPage.jsx
+++ frontend/src/Pages/Shop/ProductPage.jsx
@@ -138,6 +138,7 @@ const ProductPage = ({ onAddToCart }) => {
                             <button
                                 onClick={
                                     () => handleAddClick(product)
+                                    
                                     // onAddToCart(product)
                                 }
                                 className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 transition-colors rounded"
